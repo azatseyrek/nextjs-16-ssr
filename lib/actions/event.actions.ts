1e8eb3582deb21b1b34c046fd1370d02e97b6ae5
Azat Seyrek <azat.seyrek@adesso.com.tr>
2025-12-24 14:45:54 +0300

    fix: build errors fixed

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -3,6 +3,16 @@
 import Event from "@/database/event.model";
 import connectDB from "@/lib/mongodb";
 
+export const getAllEvents = async () => {
+  try {
+    await connectDB();
+    const events = await Event.find().sort({ createdAt: -1 }).lean();
+    return JSON.parse(JSON.stringify(events));
+  } catch {
+    return [];
+  }
+};
+
 export const getSimilarEventsBySlug = async (slug: string) => {
   try {
     await connectDB();
